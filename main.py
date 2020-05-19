bb93a08f518caea91e9523d6da76f9248ef5f0d6
trevor <me@trevorbehnke.com>
2020-05-19 12:51:35 -0700

    magic8ball

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from Die import Die
 from Sort import Sort
+from Magic8 import Magic8ball, response
+import random
 
 app = Flask(__name__)
 
@@ -10,6 +12,9 @@ dice_roll = Die
 global sortaword
 sortaword = Sort
 
+global magic8ball
+magic8ball = Magic8ball
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -17,7 +22,7 @@ def home_page():
 
 @app.route('/rollthebones')
 def rollthebones_page():
-    return render_template('rollthebones.html', the_title='Roll the Bones!')
+    return render_template('rollthebones.html', the_title='Roll the Dice!')
 
 @app.route('/rollresults', methods=['POST'])
 def roll_die():
@@ -52,3 +57,22 @@ def word_sort():
     sorted_word = my_word.sort_word()
 
     return render_template('sortawordresults.html', the_title=title, the_results=sorted_word)
+
+@app.route('/magic8ball')
+def magic8_page():
+    return render_template('magic8ball.html', the_title='Magic 8 Ball!')
+
+@app.route('/magic8ballresults', methods=['POST'])
+def magic_8():
+    global magic8ball
+
+    question = str(request.form['question'])
+    title = 'Answer: '
+
+    question = Magic8ball(question)
+    
+    answer = question.ask()
+    # for x in response:
+    #     return random.choice(response)
+
+    return render_template('magic8ballresults.html', the_title=title, the_results=answer)
